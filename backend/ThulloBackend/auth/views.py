ae1cc5d02eeb18ae61634ae6e0e2a86e20ed97e1
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-23 16:41:08 +0800

    Add login and logout view

--- backend/ThulloBackend/auth/views.py
+++ backend/ThulloBackend/auth/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
-# Create your views here.
+from auth.models import User
+from auth.serializers import UserSerializer, CustomTokenObtainPairSerializer
+
+class UserSignUpViewSet(ModelViewSet):
+  """
+  A viewset for User model
+  """
+  serializer_class = UserSerializer
+
+  @action(methods=['post'], detail=False)
+  def signup(self, request, pk=None):
+    serializer = UserSerializer(data=request.data)
+
+    user_data = {
+      'email': request.data['email'],
+      'username': request.data['username'],
+      'password': request.data['password']
+    }
+
+    # serializer.is_valid will catch raised errors and return false if validation fails
+    if serializer.is_valid():
+      # Create user with custom user model
+      User.objects.create_user(**user_data)
+      return Response(status=status.HTTP_200_OK)
+      
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+
+  serializer_class = CustomTokenObtainPairSerializer
+  
