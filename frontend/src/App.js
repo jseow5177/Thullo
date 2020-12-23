b10d63d239bc3272f1c900a7bb5143f69d27daec
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-23 23:27:29 +0800

    Add router and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import PrivateRoute from './modules/auth/components/PrivateRoute'
+
+import Landing from './modules/auth/views/Landing/'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <PrivateRoute exact path="/home" />
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 
