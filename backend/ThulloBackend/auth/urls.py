821c994c73115930a33de7612cf0bd344ca8dd6e
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-23 16:40:48 +0800

    Add auth urls

---
+++ backend/ThulloBackend/auth/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls import include
+
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+from auth.views import UserSignUpViewSet, EmailTokenObtainPairView
+
+router = routers.DefaultRouter()
+router.register(r'', UserSignUpViewSet, basename='auth')
+
+urlpatterns = [
+  path('login/', EmailTokenObtainPairView.as_view(), name='login'),
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  path('', include(router.urls))
+]
