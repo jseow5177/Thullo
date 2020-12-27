d7b07abaef5900d461fa6ebc7c23d145f97043ac
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-27 16:39:48 +0800

    Add auth constant file

---
+++ frontend/src/modules/auth/constants.js
@@ -0,0 +1,16 @@
+const AUTH = {
+  '/login': {
+    title: 'Log In to Thullo',
+    btnText: 'Log In',
+    linkText: 'No account? Sign up now!',
+    navigateTo: '/signup'
+  },
+  '/signup': {
+    title: 'Sign Up for Thullo',
+    btnText: 'Sign Up',
+    linkText: 'Already have an account? Log in here',
+    navigateTo: '/login'
+  }
+}
+
+export default AUTH
