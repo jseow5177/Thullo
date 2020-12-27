b56064d0660467568e307b5eab76e977813dd6c0
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-27 22:04:31 +0800

    Change zxcvbn library

--- backend/ThulloBackend/ThulloBackend/settings.py
+++ backend/ThulloBackend/ThulloBackend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth.apps.AuthConfig',
-    'zxcvbn_password'
+    'django_zxcvbn_password_validator'
 ]
 
 MIDDLEWARE = [
