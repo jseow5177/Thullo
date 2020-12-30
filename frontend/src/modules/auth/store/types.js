5ce50e0f42cf16e14dc6a4bbc7cfa554bb0aa8b3
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-30 23:45:15 +0800

    Add authentication and error action types

--- frontend/src/modules/auth/store/types.js
+++ frontend/src/modules/auth/store/types.js
@@ -1,2 +1,12 @@
 // Set the current authenticated user
 export const SET_CURRENT_USER = 'SET_CURRENT_USER'
+
+// Indicate that the backend is processing the auth request
+export const SET_AUTHENTICATING = 'SET_AUTHENTICATING'
+
+// Indicate that the backend is done processing the auth request
+export const CLEAR_AUTHENTICATING = 'CLEAR_AUTHENTICATING'
+
+export const SET_ERROR = 'SET_ERROR'
+
+export const CLEAR_ERROR = 'CLEAR_ERROR'
