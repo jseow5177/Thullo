74c52437447eef4d67929a7dcc7443997d2e2046
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-12-30 23:46:09 +0800

    Add login and signup actions

--- frontend/src/modules/auth/store/actions.js
+++ frontend/src/modules/auth/store/actions.js
@@ -1,5 +1,56 @@
-const actions = {
+import AuthService from '../services/auth.service'
+import { SET_CURRENT_USER, SET_AUTHENTICATING, CLEAR_AUTHENTICATING, SET_ERROR, CLEAR_ERROR } from './types'
+
+export const login = (email, password) => async (dispatch) => {
+
+  dispatch({ type: SET_AUTHENTICATING })
+  dispatch({ type: CLEAR_ERROR })
+
+  try {
+    const token = await AuthService.login(email, password)
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: token
+    })
+
+    return true
+  } catch (error) {
+    dispatch({
+      type: SET_ERROR,
+      payload: {
+        errorCode: error.errorCode,
+        message: error.message
+      }
+    })
+
+    return false
+  } finally {
+    dispatch({ type: CLEAR_AUTHENTICATING })
+  }
 
 }
 
-export default actions
+export const signup = (email, username, password) => async (dispatch) => {
+
+  dispatch({ type: SET_AUTHENTICATING })
+  dispatch({ type: CLEAR_ERROR })
+
+  try {
+    await AuthService.signup(email, username, password)
+
+    return true
+  } catch (error) {
+    dispatch({
+      type: SET_ERROR,
+      payload: {
+        errorCode: error.errorCode,
+        message: error.message
+      }
+    })
+    
+    return false
+  } finally {
+    dispatch({ type: CLEAR_AUTHENTICATING })
+  }
+
+}
