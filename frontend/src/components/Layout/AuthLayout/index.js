a89108524db2e6f3041a37294c646ff1486269fd
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-01 20:39:36 +0800

    Redirect user to home page if already authenticated

--- frontend/src/components/Layout/AuthLayout/index.js
+++ frontend/src/components/Layout/AuthLayout/index.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import styles from './AuthLayout.module.scss'
+
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 import AuthHeader from '../../Header/AuthHeader'
 
-function AuthLayout({ children }) {
+function AuthLayout({ children, auth }) {
+
+  const [isInitialised, setIsInitialised] = useState(false)
+  const [redirectPath, setRedirectPath] = useState(null)
+
+  useEffect(() => {
+    if (auth.isAuthenticated) {
+      setRedirectPath('/home')
+    }
+    setIsInitialised(true)
+  }, [auth.isAuthenticated])
+
+  const render = () => {
+    if (isInitialised && redirectPath) {
+      return <Redirect to={redirectPath} />
+    } else if (isInitialised && redirectPath === null) {
+      return (
+        <>
+          <AuthHeader />
+          {children}
+        </>
+      )
+    } else {
+      return <CircularProgress size={50} className={styles.spinner} />
+    }
+  }
+
   return (
     <div className={styles.layout}>
-      <AuthHeader />
-      {children}
+      {render()}
     </div>
   )
 }
 
-export default AuthLayout
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, null)(AuthLayout)
