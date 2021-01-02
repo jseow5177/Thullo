d5c1287ca5c0dd426a9d1396a5b6d284f51b3371
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-02 22:54:08 +0800

    Add mount and unmount 401 interceptor api service

--- frontend/src/common/services/api.service.js
+++ frontend/src/common/services/api.service.js
@@ -1,7 +1,14 @@
-import TokenService from './token.service'
 import axios from 'axios'
 
+import TokenService from './token.service'
+import store from '../../store'
+import { refreshToken, logout } from '../../modules/auth/store/actions'
+
+
 const ApiService = {
+
+  // Stores the 401 interceptor position so that it can later be ejected
+  _401interceptor: null,
 
   setAuthHeader() {
     /**
@@ -28,6 +35,74 @@ const ApiService = {
 
   delete(resource) {
     return axios.delete(resource)
+  },
+
+  mount401Interceptor: function mount401Interceptor() {
+    // Add a response interceptor
+    axios.interceptors.response.use(function (response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      return response
+    }, async function (error) {
+      /**
+       * Any status code that falls outside the range of 2xx cause this function to trigger
+       * 
+       * Intercepts and handle 401 token errors
+       * 1. If server responds with a 401 error, attempt to get new access token and resend original request
+       * 2. If token refresh fails, proceed to logout
+       */
+
+      if (error.response && error.response.status === 401) {
+        // If access token is invalid / expired
+        if (error.response.data.code === 'token_not_valid' && !error.config.url.includes('/auth/refresh')) {
+          try {
+            // Attempt to get new access token via refresh token
+            await store.dispatch(refreshToken())
+
+            const { method, url, data, params, responseType } = error.response.config
+
+            const config = {
+              method: method, // HTTP method
+              url: url // Request url
+            }
+
+            // Check if there are any post data
+            if (typeof data !== 'undefined') {
+              try {
+                config.data = JSON.parse(data)
+              } catch {
+                config.data = data
+              }
+            }
+
+            // Check if there are any url params
+            if (typeof params !== 'undefined') {
+              config.params = params
+            }
+
+            // Check if there is a specified response type
+            if (typeof responseType !== 'undefined') {
+              config.responseType = responseType
+            }
+
+            // Attempt to resend the original request
+            return axios(config)
+          } catch {
+            // Refresh token fails
+            store.dispatch(logout())
+            return Promise.reject(error)
+          }
+        }
+      }
+
+      // Return any other error
+      return Promise.reject(error)
+    })
+  },
+
+  unmount401Interceptor() {
+    // Eject the interceptor
+    axios.interceptors.response.eject(this._401interceptor)
+    this._401interceptor = null
   }
 
 }
