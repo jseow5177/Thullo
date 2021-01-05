381f9c02955b666910d5e742354653acb1827a56
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-05 21:58:02 +0800

    Basic MainLayout setup with header

--- frontend/src/components/Layout/MainLayout/index.js
+++ frontend/src/components/Layout/MainLayout/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import MainHeader from '../Header/MainHeader'
+import MainHeader from '../../Header/MainHeader'
+
+import styles from './MainLayout.module.scss'
 
 function MainLayout({ children }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <MainHeader />
       {children}
     </div>
