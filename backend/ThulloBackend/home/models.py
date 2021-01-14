bcb83fa86e619df6256e7ba7f9ca5af5b97c8e35
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-14 12:19:22 +0800

    Add board model

--- backend/ThulloBackend/home/models.py
+++ backend/ThulloBackend/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from auth.models import User
+
+class Board(models.Model):
+  title = models.CharField(max_length=50, blank=False, null=False)
+  about = models.TextField(max_length=200, blank=True, null=False)
+  cover_photo = models.BinaryField(blank=True, null=False)
+  color = models.CharField(max_length=7, blank=False, null=False)
+
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    verbose_name = "Board"
+
+
+
