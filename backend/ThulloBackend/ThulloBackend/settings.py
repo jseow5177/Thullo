6a495b6b9b6b9227eaa31ef17a67d39be3956554
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-14 12:19:07 +0800

    Register new home app

--- backend/ThulloBackend/ThulloBackend/settings.py
+++ backend/ThulloBackend/ThulloBackend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth.apps.AuthConfig',
+    'home.apps.HomeConfig',
     'django_zxcvbn_password_validator'
 ]
 
