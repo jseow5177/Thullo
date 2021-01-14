1da369a4cef17b88aefcda4b35aca3974acc0798
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-14 12:19:50 +0800

    Add BoardViewSet

--- backend/ThulloBackend/home/views.py
+++ backend/ThulloBackend/home/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
-# Create your views here.
+from home.serializers import BoardSerializer
+from home.models import Board
+
+class BoardViewSet(ModelViewSet):
+  """
+  A viewset to add and retrieve boards
+  """
+
+  serializer_class = BoardSerializer
+
+  def create(self, request):
+
+    current_user = request.user
+
+    board_data = {
+      "title": request.data.get("title"),
+      "about": request.data.get("about"),
+      "color": request.data.get("color"),
+      "cover_photo": request.data.get("cover_photo"),
+      "owner": current_user.id # Add owner field
+    }
+
+    # Check if data is valid with serializer
+    serializer = self.get_serializer(data=board_data)
+    serializer.is_valid(raise_exception=True)
+
+    # Save board. Errors will be thrown and returned within save()
+    instance = serializer.save()
+
+    return Response(data=instance, status=status.HTTP_201_CREATED)
