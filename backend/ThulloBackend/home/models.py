2fcef27bd5adc4805c8c2123bc39e8cfbf06c0c6
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-17 11:35:07 +0800

    Change BinaryField name and make it editable

--- backend/ThulloBackend/home/models.py
+++ backend/ThulloBackend/home/models.py
@@ -5,7 +5,7 @@ from auth.models import User
 class Board(models.Model):
   title = models.CharField(max_length=50, blank=False, null=False)
   about = models.TextField(max_length=200, blank=True, null=False)
-  cover_photo = models.BinaryField(blank=True, null=False)
+  image = models.BinaryField(blank=True, null=False, editable=True)
   color = models.CharField(max_length=7, blank=False, null=False)
 
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
