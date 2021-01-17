3a46e2096dd638a8d36eb85fc1fd8b75bab3af39
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-17 10:23:25 +0800

    Size of loading spinner to be 80% of button

--- frontend/src/components/CustomMaterialUI/LoadingButton.js
+++ frontend/src/components/CustomMaterialUI/LoadingButton.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
 
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 function LoadingButton({ children, pending, ...props }) {
+  const [btnSize, setBtnSize] = useState(0)
+  const btnRef = useRef(null)
+
+  useEffect(() => {
+    const { clientHeight, clientWidth } = btnRef.current
+    setBtnSize(Math.min(clientHeight, clientWidth))
+  }, [])
+
   return (
-    <Button {...props}>
-      { pending
-        ? <CircularProgress size={25} style={{ color: '#FFF' }} />
-        : children
+    <Button {...props} ref={btnRef}>
+      {
+        pending
+          ? <CircularProgress size={0.75 * btnSize} style={{ color: '#FFF' }} />
+          : children
       }
     </Button>
   )
+}
+
+LoadingButton.defaultProps = {
+  pending: false
+}
+
+LoadingButton.propTypes = {
+  pending: PropTypes.bool,
 }
 
 export default LoadingButton
