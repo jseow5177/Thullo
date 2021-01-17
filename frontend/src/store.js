4b111d96a27262d9bda10b2d743a329b34b2fd71
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-17 12:49:14 +0800

    Connect to store of home module

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,16 @@ import thunk from 'redux-thunk'
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 
 import { authReducers, authState } from './modules/auth/store'
+import { homeReducers, homeState } from './modules/home/store'
 
 const rootReducer = combineReducers({
-  auth: authReducers
+  auth: authReducers,
+  home: homeReducers
 })
 
 const initialState = {
-  auth: authState
+  auth: authState,
+  home: homeState
 }
 
 const middleware = [thunk]
