7f47f1e06ac92a7d950a092551490da185ef927d
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-17 12:50:49 +0800

    Add reducer for home module

--- frontend/src/modules/home/store/reducers.js
+++ frontend/src/modules/home/store/reducers.js
@@ -1,7 +1,27 @@
+import {
+  ADD_BOARDS,
+  SET_ADD_BOARD_LOADING,
+  CLEAR_ADD_BOARD_LOADING
+} from './types'
 import initialState from './state'
 
 const reducers = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_BOARDS:
+      return {
+        ...state,
+        boards: [...state.boards, action.payload]
+      }
+    case SET_ADD_BOARD_LOADING:
+      return {
+        ...state,
+        addBoardLoading: true
+      }
+    case CLEAR_ADD_BOARD_LOADING:
+      return {
+        ...state,
+        addBoardLoading: false
+      }
     default:
       return state
   }
