2f709425262e76eb465e36886f57d0f41aaf6d92
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-20 23:50:12 +0800

    Show linear progress when loading

--- frontend/src/components/Layout/MainLayout/index.js
+++ frontend/src/components/Layout/MainLayout/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 import MainHeader from '../../Header/MainHeader'
 
 import styles from './MainLayout.module.scss'
 
-function MainLayout({ children }) {
+function MainLayout({ children, home }) {
   return (
     <div className={styles.layout}>
       <MainHeader />
+      {home.getBoardsLoading && <LinearProgress color="primary" />}
       {children}
     </div>
   )
 }
 
-export default MainLayout
+const mapStateToProps = (state) => ({
+  home: state.home
+})
+
+export default connect(mapStateToProps, null)(MainLayout)
