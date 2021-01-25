0e9f7967a1f799b5d9512547705c83abddfd45c8
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-25 21:53:35 +0800

    Wrap board component with SortableElement. Add more stylings logic

--- frontend/src/modules/home/components/Board/index.js
+++ frontend/src/modules/home/components/Board/index.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SortableElement } from 'react-sortable-hoc'
 
 // Material UI Components
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Image from '../../../../components/Image'
+
+import DefaultImage from '../../../../assets/image/default.png'
 
 import styles from './Board.module.scss'
 
-const Board = SortableElement(({ board }) => (
-  <div>
-    <Card className={styles.root}>
-      <CardHeader title={board.title} />
-      <CardContent>
-        {board.about}
-      </CardContent>
-    </Card>
-  </div>
-))
+const Board = SortableElement(({ board }) => {
+  const [raised, setRaised] = useState(false)
+
+  const imgSrc = board.image
+    ? `data:image/png;base64,${board.image}`
+    : DefaultImage
+
+  const handleMouseOver = () => {
+    setRaised(true)
+  }
+
+  const handleMouseOut = () => {
+    setRaised(false)
+  }
+
+  return (
+    <div className={styles.root}>
+      <Card
+        className={styles.card}
+        style={{ backgroundColor: board.color }}
+        raised={raised}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <Image
+          src={imgSrc}
+          caption="board img"
+          height="11rem"
+        />
+        <CardHeader
+          title={
+            <Typography variant="h5">
+              {board.title}
+            </Typography>
+          }
+        />
+      </Card>
+    </div>
+  )
+})
 
 export default Board
