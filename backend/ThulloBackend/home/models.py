fb576c26284454c296f4bfdb6d9602b073b42e52
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-01-26 22:47:15 +0800

    Add order as PositiveIntegerField

--- backend/ThulloBackend/home/models.py
+++ backend/ThulloBackend/home/models.py
@@ -7,6 +7,7 @@ class Board(models.Model):
   about = models.TextField(max_length=200, blank=True, null=False)
   image = models.BinaryField(blank=True, null=False, editable=True)
   color = models.CharField(max_length=7, blank=False, null=False)
+  order = models.PositiveIntegerField(blank=False, null=False)
 
   owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
