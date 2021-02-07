5845840d4c0c41f5f5ca0afed3884da5e22896eb
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-07 09:12:39 +0800

    Create ListViewSet

--- backend/ThulloBackend/home/views.py
+++ backend/ThulloBackend/home/views.py
@@ -5,12 +5,13 @@ from rest_framework.decorators import action
 
 from django.db.models import F
 
-from home.serializers import BoardSerializer
+from home.serializers import BoardSerializer, ListSerializer
 from home.models import Board
+from home.models import List
 
 class BoardViewSet(ModelViewSet):
   """
-  A viewset to add and retrieve boards
+  A viewset for boards
   """
 
   serializer_class = BoardSerializer
@@ -24,19 +25,6 @@ class BoardViewSet(ModelViewSet):
     serializer = self.get_serializer(user_boards, many=True)
 
     return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-  @action(methods=['put'], detail=False)
-  def switch_order(self, request):
-    boardId = request.data['board']
-    boardOrder = request.data['order']
-
-    # Increment the order of boards after the dragged board
-    Board.objects.filter(order__gte=boardOrder).update(order=F('order') + 1)
-
-    # Update order of board
-    Board.objects.filter(pk=boardId).update(order=boardOrder)
-
-    return Response(status=status.HTTP_200_OK)
 
   def create(self, request):
 
@@ -62,7 +50,54 @@ class BoardViewSet(ModelViewSet):
     serializer = self.get_serializer(data=board_data)
     serializer.is_valid(raise_exception=True)
 
-    # Save board. Errors will be thrown and returned within save()
+    # Save board
+    serializer.save()
+
+    return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+  @action(methods=['put'], detail=False)
+  def switch_order(self, request):
+    """
+    Switch the order of boards when a board is dragged to another position
+    """
+    boardId = request.data['board']
+    boardOrder = request.data['order']
+
+    # Increment the order of boards after the dragged board
+    Board.objects.filter(order__gte=boardOrder).update(order=F('order') + 1)
+
+    # Update order of board
+    Board.objects.filter(pk=boardId).update(order=boardOrder)
+
+    return Response(status=status.HTTP_200_OK)
+
+class ListViewSet(ModelViewSet):
+  """
+  A viewset for lists
+  """
+  serializer_class = ListSerializer
+
+  def get_queryset(self):
+    queryset = List.objects.all()
+    return queryset
+  
+  def list(self, request):
+    boardId = request.GET.get('boardId', '')
+
+    board_lists = self.get_queryset().filter(board=boardId)
+    serializer = self.get_serializer(board_lists, many=True)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+  def create(self, request):
+    """
+    Create a list for a board
+    """
+    # Check if data is valid with serializer
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    # Save list
     serializer.save()
 
     return Response(data=serializer.data, status=status.HTTP_201_CREATED)
