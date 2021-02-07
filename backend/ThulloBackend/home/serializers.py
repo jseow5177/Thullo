40c5661facd98a119f5298f463a346b577b19066
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-07 09:13:59 +0800

    Add ListSerializer

--- backend/ThulloBackend/home/serializers.py
+++ backend/ThulloBackend/home/serializers.py
@@ -1,8 +1,14 @@
-from home.models import Board
+from home.models import Board, List
 from rest_framework import serializers
 
 class BoardSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = Board
+    fields = "__all__"
+
+class ListSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = List
     fields = "__all__"
