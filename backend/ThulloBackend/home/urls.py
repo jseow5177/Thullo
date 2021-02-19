5519a19a9cb87d6ce3d66fa63c2f48382c2799f3
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-19 15:40:17 +0800

    Add API to add a new label

--- backend/ThulloBackend/home/urls.py
+++ backend/ThulloBackend/home/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import include
 
 from rest_framework import routers
 
-from home.views import BoardViewSet, ListViewSet
+from home.views import BoardViewSet, ListViewSet, LabelViewSet
 
 router = routers.DefaultRouter()
 router.register(r'board', BoardViewSet, basename='board')
 router.register(r'list', ListViewSet, basename='list')
+router.register(r'label', LabelViewSet, basename='label')
 
 urlpatterns = router.urls
