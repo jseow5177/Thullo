db47f0c6217edc74f645f5ca101f9ecf4d76e0af
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-02-25 22:37:52 +0800

    Add url to CardViewSet

--- backend/ThulloBackend/home/urls.py
+++ backend/ThulloBackend/home/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import include
 
 from rest_framework import routers
 
-from home.views import BoardViewSet, ListViewSet, LabelViewSet
+from home.views import BoardViewSet, ListViewSet, LabelViewSet, CardViewSet
 
 router = routers.DefaultRouter()
 router.register(r'board', BoardViewSet, basename='board')
 router.register(r'list', ListViewSet, basename='list')
 router.register(r'label', LabelViewSet, basename='label')
+router.register(r'card', CardViewSet, basename='card')
 
 urlpatterns = router.urls
