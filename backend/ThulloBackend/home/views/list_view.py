39c3a64cf187edb8ac528b64932216d411857b97
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-03-10 12:30:23 +0800

    Add API endpoints to delete and update list

--- backend/ThulloBackend/home/views/list_view.py
+++ backend/ThulloBackend/home/views/list_view.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
+from django.db import transaction
 from django.db.models import F
 
 from home.models import List
@@ -12,6 +13,7 @@ class ListViewSet(ModelViewSet):
   """
   A viewset for lists
   """
+  queryset = List.objects.all()
 
   def create(self, request):
     """
@@ -29,6 +31,35 @@ class ListViewSet(ModelViewSet):
     serializer.save()
 
     return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+  
+  def update(self, request, pk=None):
+    """
+    Update an existing list of a board
+    """
+    board_list = List.objects.get(pk=pk)
+
+    # Check if data is valid with serializer
+    serializer = ListSerializer(board_list, data=request.data, exclude_fields=['order', 'board'])
+    serializer.is_valid(raise_exception=True)
+
+    # Save existing list
+    serializer.save()
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+  @transaction.atomic
+  def destroy(self, request, pk=None):
+    """
+    Delete an existing list of a board
+    """
+    board_list = List.objects.get(pk=pk)
+
+    # Bring all lists after the deleted list forward
+    List.objects.filter(board_id=board_list.board.id, order__gt=board_list.order).update(order=F('order') - 1)
+
+    board_list.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
   @action(methods=['put'], detail=False)
   def switch_order(self, request):
